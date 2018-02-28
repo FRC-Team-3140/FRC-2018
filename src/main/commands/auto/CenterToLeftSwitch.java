127bcc7397b90b2e9595218e88cdc4c3ea9f6c6b
CryptoMiner <CryptoMiner@192.168.2.2>
2018-02-28 05:13:31 -0800

    Psuedo Commented what needs to be written in the remaining auto modes

--- src/main/commands/auto/CenterToLeftSwitch.java
+++ src/main/commands/auto/CenterToLeftSwitch.java
@@ -4,7 +4,14 @@ import interfacesAndAbstracts.ImprovedCommandGroup;
 
 public class CenterToLeftSwitch extends ImprovedCommandGroup {
 	public CenterToLeftSwitch() {
-		//TODO
+	//DriveForward(Break away from wall so there is no resistance on the first turn)
+	//TurnLeft
+	//LiftElevator
+	//DriveForward
+	//TurnRight
+	//DriveForward
+	//Drop
+	//Wait
+	//Close
 	}
-
 }
--- src/main/commands/auto/CenterToRightSwitch.java
+++ src/main/commands/auto/CenterToRightSwitch.java
@@ -4,6 +4,14 @@ import interfacesAndAbstracts.ImprovedCommandGroup;
 
 public class CenterToRightSwitch extends ImprovedCommandGroup {
 	public CenterToRightSwitch() {
-		//TODO		
+	//DriveForward(Break away from wall so there is no resistance on the first turn)
+	//TurnRight
+	//LiftElevator
+	//DriveForward
+	//TurnLeft
+	//DriveForward
+	//Drop
+	//Wait
+	//Close	
 	}
 }
