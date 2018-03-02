0410bc00763ca633b4a6c4793d82eed4e2626c43
encrxpted <rulangu@gmail.com>
2018-03-02 12:27:56 -0500

    delayed arm close before tilt up

--- src/main/commands/commandgroups/cubeManipulator/IntakeCubeOff.java
+++ src/main/commands/commandgroups/cubeManipulator/IntakeCubeOff.java
@@ -9,6 +9,7 @@ import main.commands.pneumatics.tilt.TiltUp;
 public class IntakeCubeOff extends ImprovedCommandGroup {
 	public IntakeCubeOff() {
 		addSequential(new ArmClose());
+		addSequential(new WaitCommand(0.3));
     	addSequential(new TiltUp());
     	addSequential(new WaitCommand(2.5));
     	addSequential(new SpinOff());
