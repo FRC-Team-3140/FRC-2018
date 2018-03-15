39056675e18a7779bb37b3d3290b475f32f1d4fd
TheSuperDuck <TheSuperDuck@users.noreply.github.com>
2018-03-15 11:43:59 -0400

    Cleaned up
    In src/main/commands/pneumatics/shift/Shift.java:
    	Marked internal value as final

--- src/main/commands/pneumatics/shift/Shift.java
+++ src/main/commands/pneumatics/shift/Shift.java
@@ -5,38 +5,20 @@ import interfacesAndAbstracts.ImprovedCommand;
 import main.Robot;
 
 class Shift extends ImprovedCommand {
-
-	private DoubleSolenoid.Value v;
+	private final DoubleSolenoid.Value v;
+	
     public Shift(DoubleSolenoid.Value v) {
     	requires(Robot.pn);
     	this.v = v;
     }
 
-    // Called just before this Command runs the first time
-    @Override
-	protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
     @Override
 	protected void execute() {
     	Robot.pn.shift(v);
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     @Override
 	protected boolean isFinished() {
         return true;
-    }
-
-    // Called once after isFinished returns true
-    @Override
-	protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    @Override
-	protected void interrupted() {
     }
 }
