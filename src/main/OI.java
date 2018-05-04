da0e75299189f33ca4500942ce6d9d6316f710f8
Programming Team <Programming Team@DESKTOP-MA5TGTN.knoxschools.ad>
2018-05-04 15:53:07 -0400

    removed stuff with non-drivetrain subsystems and all autos

--- src/main/OI.java
+++ src/main/OI.java
@@ -17,39 +17,6 @@ import main.commands.pneumatics.tilt.TiltUp;
 public class OI extends ImprovedClass {	
 	public OI() {
 		xbox.setInternalControl(false);
-		xbox2.setInternalControl(false);
-		
-		// Shoots out without elevator
-		xbox.leftJoystickPress.whenPressed(new ShiftUp());
-		xbox.leftJoystickPress.whenReleased(new ShiftDown());
-		//xbox.a.whenPressed(new SpinIn());
-		
-		//xbox.a.whenPressed(new DistanceDriveStraight(600));
-		/*
-		// auto testing calibration
-		xbox.a.whenPressed(new TimedTurn(TurnMode.Left, timedTurnPercent, timedTurn90degTime));
-		xbox.x.whenPressed(new TimedTurn(TurnMode.Left, timedTurnPercent, timedTurn45degTime));
-		xbox.y.whenPressed(new TimedDrive(timedDrivePercent, 2.5));
-	    */
-		
-		//xbox.x.whenPressed(new DistanceDriveStraight(84));
-		//xbox.b.whenPressed(new DistanceDriveStraight(-12));
-		//xbox.a.whenPressed(new TurnToAngle(90));
-		//xbox.y.whenPressed(new TestAutoTurning());
-		/*
-		xbox2.a .whenPressed(new MoveToBottom(1.5));
-		xbox2.b.whenPressed(new MoveToSwitch(1.5));
-		xbox2.y.whenPressed(new MoveToTop(3));
-	*/
-		//xbox.b.whenPressed(new TurnToAngle(90));
-		//xbox.a.whenPressed(new MoveToBottom(1.5));
-		//xbox.y.whenPressed(new MoveToSwitch(1.5));
-		//xbox.rightBumper.whenPressed(new MoveToTop(3));
-		//xbox.leftBumper.whenPressed(new AltRightToRightSwitch());
-
-		
-		xbox2.leftBumper.whenPressed(new SwitchTilt(new TiltDown(), new TiltUp()));
-		xbox2.rightBumper.whenPressed(new SwitchArm(new ArmOpen(), new ArmClose()));
 	}
 	
 	public static XboxController getXbox() {
@@ -75,16 +42,8 @@ public class OI extends ImprovedClass {
 		xbox.setButtonStatus(a, b, x, y, leftBumper, rightBumper, select, start, leftJoystickPress, rightJoystickPress, leftTrigger, rightTrigger);
 	}
 	
-	public void setButtonValues2(boolean a, boolean b, boolean x, boolean y, boolean leftBumper, boolean rightBumper,
-			boolean select, boolean start, boolean leftJoystickPress, boolean rightJoystickPress,
-			boolean leftTrigger, boolean rightTrigger) {
-		xbox2.setInternalControl(true);
-		xbox2.setButtonStatus(a, b, x, y, leftBumper, rightBumper, select, start, leftJoystickPress, rightJoystickPress, leftTrigger, rightTrigger);
-	}
-	
 	public void setInternalControl(boolean internalControl) {
 		xbox.setInternalControl(internalControl);
-		xbox2.setInternalControl(internalControl);
 	}
 }
  
--- src/main/Robot.java
+++ src/main/Robot.java
@@ -2,91 +2,28 @@
 
 package main;
 
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import interfacesAndAbstracts.ImprovedRobot;
-import main.commands.auto.Baseline;
-import main.commands.altermativeAuto.AltCenterToLeftSwitch;
-import main.commands.altermativeAuto.AltCenterToRightSwitch;
-import main.commands.altermativeAuto.AltLeftToLeftScale;
-import main.commands.altermativeAuto.AltLeftToLeftSwitch;
-import main.commands.altermativeAuto.AltLeftToRightSwitch;
-import main.commands.altermativeAuto.AltRightToLeftSwitch;
-import main.commands.altermativeAuto.AltRightToRightScale;
-import main.commands.altermativeAuto.AltRightToRightSwitch;
-import main.commands.auto.DoNothing;
-import main.commands.auto.ResetForTeleop;
-import main.commands.drivetrain.TimedTankDriveStraight;
 import main.subsystems.DriverCamera;
 import main.subsystems.Drivetrain;
-import main.subsystems.Elevator;
-import main.subsystems.Intake;
-import main.subsystems.Pneumatics;
-
 public class Robot extends ImprovedRobot {
 	public static Drivetrain dt;
-	public static Pneumatics pn;
-	public static Intake in;
-	public static Elevator el;
 	public static DriverCamera dc;
 	public static OI oi;
-	
-	// AUTO LOGIC
-	private enum StartPos {LEFT, CENTER, RIGHT}
-	private enum RobotAction {DO_NOTHING, BASELINE, SWITCH, SCALE}
-	public static StartPos start_pos;
-	public static RobotAction robot_act;
-	private static SendableChooser<RobotAction> autoChooser;
-	private static SendableChooser<StartPos> startPos;
-	private static Command autoCommand;
-	
-	class AutoCommandGroup extends CommandGroup {
-		public AutoCommandGroup(Command auto, boolean reset, boolean moveDown) {
-			addSequential(auto);
-			if(reset) addSequential(new ResetForTeleop(moveDown));
-		}
-	}
 	
 	@Override
 	public void robotInit() {
 		// OI must be at end
 		dt = new Drivetrain();
-		pn = new Pneumatics();
-		in = new Intake();
-		el = new Elevator();
 		oi = new OI();
 		dc = new DriverCamera();
-		// Auto modes
-		autoChooser = new SendableChooser<>();
-		autoChooser.addDefault("Do Nothing", RobotAction.DO_NOTHING);
-		autoChooser.addObject("Baseline", RobotAction.BASELINE);
-		autoChooser.addObject("Switch Priority", RobotAction.SWITCH);
-		autoChooser.addObject("Scale Priority", RobotAction.SCALE);
-		// Starting Pos
-		startPos = new SendableChooser<>();
-		startPos.addDefault("Left", StartPos.LEFT);
-		startPos.addObject("Center", StartPos.CENTER);
-		startPos.addObject("Right", StartPos.RIGHT);
-		SmartDashboard.putData("Starting Position", startPos);
-		SmartDashboard.putData("Auto Mode", autoChooser);
-		//Auto Features to Disable
-		SmartDashboard.putBoolean("Disable Scale Auto", true);
-		SmartDashboard.putBoolean("Disable Switch From Behind", false);
-		
 		//Robot Self Test
-		SmartDashboard.putData("Robot Self Test", new RobotSelfTest());
 		SmartDashboard.putData("Robot Drive Train Test", new RobotDriveTrainTest());
-		SmartDashboard.putData("Robot Drive Straight Test", new TimedTankDriveStraight(1, 1, 5));
 	}
 	
 	@Override
 	public void disabledInit() {
-		if(autoCommand != null && autoCommand.isRunning())
-			autoCommand.cancel();
 	}
 	
 	public void disabledPeriodic() {
@@ -95,101 +32,7 @@ public class Robot extends ImprovedRobot {
 	}
 
 	@Override
-	public void autonomousInit() {		
-		String gmsg = DriverStation.getInstance().getGameSpecificMessage();
-		while (gmsg == null || gmsg.length() != 3) {
-			gmsg = DriverStation.getInstance().getGameSpecificMessage();
-			try {
-				Thread.sleep(5);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		System.out.println("message" + gmsg);
-		System.out.println("auto" + autoChooser.getSelected());
-		System.out.println("pos" + startPos.getSelected());
-
-		boolean leftSwitch = gmsg.charAt(0) == 'L';
-		boolean leftScale = gmsg.charAt(1) == 'L';
-		boolean scaleDisabled = true;
-		boolean behindSwitchDisabled = false;
-			
-		boolean isSwitch = false;
-		start_pos = startPos.getSelected();
-		robot_act = autoChooser.getSelected();
-		
-		if(robot_act == RobotAction.DO_NOTHING)//Do Nothing
-			autoCommand = new DoNothing();
-		else if(robot_act == RobotAction.BASELINE)//Baseline
-			autoCommand = new Baseline();
-		else if(robot_act == RobotAction.SWITCH){//Priority Switch
-			if(start_pos == StartPos.LEFT) {
-				if(leftSwitch) {
-					isSwitch = true;
-					autoCommand = new AltLeftToLeftSwitch();
-				}
-				else if(!behindSwitchDisabled) {
-					isSwitch = true;
-					autoCommand = new AltLeftToRightSwitch();
-				}
-				else if(leftScale && !scaleDisabled) autoCommand = new AltLeftToLeftScale();
-				else autoCommand = new Baseline();					
-			}
-			else if(start_pos == StartPos.CENTER) {
-				isSwitch = true;
-				if(leftSwitch) autoCommand = new AltCenterToLeftSwitch();
-				else autoCommand = new AltCenterToRightSwitch();
-			}
-			else if(start_pos == StartPos.RIGHT) {
-				if(!leftSwitch) {
-					isSwitch = true;
-					autoCommand = new AltRightToRightSwitch();
-				}
-				else if(!behindSwitchDisabled) {
-					isSwitch = true;
-					autoCommand = new AltRightToLeftSwitch();
-				}
-				else if(!leftScale && !scaleDisabled) autoCommand = new AltRightToRightScale();
-				else autoCommand = new Baseline();					
-			}
-		}
-		else {//Priority Scale
-			if(start_pos == StartPos.LEFT) {
-				if(leftScale && !scaleDisabled) autoCommand = new AltLeftToLeftScale();
-				else if(leftSwitch) {
-					isSwitch = true;
-					autoCommand = new AltLeftToLeftSwitch();
-				}
-				else if(!behindSwitchDisabled) {
-					isSwitch = true;
-					autoCommand = new AltLeftToRightSwitch();
-				}
-				else autoCommand = new Baseline();					
-			}
-			else if(start_pos == StartPos.CENTER) {
-				isSwitch = true;
-				if(leftSwitch) autoCommand = new AltCenterToLeftSwitch();
-				else autoCommand = new AltCenterToRightSwitch();
-			}
-			else if(start_pos == StartPos.RIGHT) {
-				if(!leftScale && !scaleDisabled) autoCommand = new AltRightToRightScale();
-				else if(!leftSwitch) {
-					isSwitch = true;
-					autoCommand = new AltRightToRightSwitch();
-				}
-				else if(!behindSwitchDisabled) {
-					isSwitch = true;
-					autoCommand = new AltRightToLeftSwitch();
-				}
-				else autoCommand = new Baseline();				
-			}
-		}
-		
-		if(autoCommand != null)
-			(autoCommand = new AutoCommandGroup(
-					autoCommand, !(autoCommand instanceof Baseline || 
-					autoCommand instanceof DoNothing), isSwitch)).start();		
+	public void autonomousInit() {
 	}
 	
 	@Override
@@ -200,8 +43,6 @@ public class Robot extends ImprovedRobot {
 
 	@Override
 	public void teleopInit() {
-		if(autoCommand != null && autoCommand.isRunning())
-			autoCommand.cancel();
 	}	
 
 	@Override
@@ -219,9 +60,6 @@ public class Robot extends ImprovedRobot {
 		//Runtime runtime = Runtime.getRuntime();
 		SmartDashboard.updateValues();
 		dt.check();
-		pn.check();
-		in.check();
-		el.check();
 		oi.check();
 		//SmartDashboard.putNumber("Free memory", runtime.freeMemory());
 		//SmartDashboard.putNumber("Total memory", runtime.totalMemory());
@@ -236,11 +74,5 @@ public class Robot extends ImprovedRobot {
 		SmartDashboard.putNumber("DriveTrain Distance", dt.getDistanceTravelled());
 		// DriveTrain Gyro
 		SmartDashboard.putNumber("NavX Heading", dt.getHeading());
-		// Limit Switch States
-		SmartDashboard.putBoolean("First Stage Bottom", !stage1BottomSwitch.get());
-		SmartDashboard.putBoolean("First Stage Top", !stage1TopSwitch.get());
-		// Elevator encoder
-		SmartDashboard.putNumber("Elevator encoder inches", el.getDistanceTravelled());
-		SmartDashboard.putNumber("Elevator Input", elevatorMaster.get());
 	}
 }
