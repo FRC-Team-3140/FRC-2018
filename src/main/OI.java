5fd3970555470bcd76bf0024727107c0d3ac0a6c
Programming Team <Programming Team@DESKTOP-MA5TGTN.knoxschools.ad>
2018-05-04 16:03:59 -0400

    cleared errors

--- src/main/OI.java
+++ src/main/OI.java
@@ -2,17 +2,6 @@ package main;
 
 import interfacesAndAbstracts.ImprovedClass;
 import lib.joystick.XboxController;
-import main.commands.elevator.MoveToTop;
-import main.commands.intake.SpinIn;
-import main.commands.intake.SpinOut;
-import main.commands.pneumatics.arm.ArmClose;
-import main.commands.pneumatics.arm.ArmOpen;
-import main.commands.pneumatics.arm.SwitchArm;
-import main.commands.pneumatics.shift.ShiftDown;
-import main.commands.pneumatics.shift.ShiftUp;
-import main.commands.pneumatics.tilt.SwitchTilt;
-import main.commands.pneumatics.tilt.TiltDown;
-import main.commands.pneumatics.tilt.TiltUp;
 
 public class OI extends ImprovedClass {	
 	public OI() {
@@ -23,13 +12,8 @@ public class OI extends ImprovedClass {
 		return xbox;
 	}
 	
-    public static XboxController getXbox2() {
-		return xbox2;
-	}
-    
 	public void check() {
 		xbox.check();
-		xbox2.check();
 	}
 	
 	/**************
