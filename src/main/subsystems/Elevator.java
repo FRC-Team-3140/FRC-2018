56fea5caedff8f3e60259624b5c9c63f5a52925d
encrxpted <rulangu@gmail.com>
2018-05-17 20:57:05 -0400

    potential fix for elevator problems

--- src/main/subsystems/Elevator.java
+++ src/main/subsystems/Elevator.java
@@ -205,10 +205,10 @@ public class Elevator extends ImprovedSubsystem {
 	
 	public void move(double throttle) {
 		SmartDashboard.putNumber("Elevator Input", throttle);
-		if((isArmAtTop() && throttle < 0) || (isArmAtBottom() && throttle > 0))
+		if((isArmAtTop() && throttle > 0) || (isArmAtBottom() && throttle < 0))
 			throttle = 0.0;
 		if (isCompetitionRobot)
-			elevatorMaster.set(-driveHelper.handleOverPower(throttle));
+			elevatorMaster.set(driveHelper.handleOverPower(throttle));
 		else
 			elevatorMaster.set(driveHelper.handleOverPower(throttle));
 	}
